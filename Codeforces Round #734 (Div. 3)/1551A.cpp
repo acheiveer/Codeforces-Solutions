7c9fc41a17d50dd3b842a2d5e7c97ce571773949
Prabhakar kumar <67807006+acheiveer@users.noreply.github.com>
2021-08-31 20:22:24 +0530

    Create 1551A.cpp

---
+++ Codeforces Round #734 (Div. 3)/1551A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	 long long int t;
+	cin>>t;
+	while(t--){
+		long long int n,c1,c2;
+		cin>>n;
+		if(n%3==0){
+			c1=n/3;
+			c2=c1;
+		}
+		else if(n%3==1){
+			c2=n/3;
+			c1=c2+1;
+		}
+		else{
+			c1=n/3;
+			c2=c1+1;
+		}
+	
+		cout<<c1<<" "<<c2<<endl;;
+	}
+ 
+}
